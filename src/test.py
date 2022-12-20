2bb377aea1eefde2ba0b9f81fc3383c27465844d
velofrog <michael@velofrog.com>
2022-12-20 06:23:21 +0000

    Updates

--- src/test.py
+++ src/test.py
@@ -4,7 +4,10 @@ officecharts.create_linechart(officecharts.data, title="My Chart", axis_y_title=
 
 officecharts.create_linechart(officecharts.data, title="My Chart", axis_y_title="Cumulative",
                               label_endpoints=True,
-                              theme=officecharts.Theme(font=officecharts.Font(family="Times New Roman"),
+                              theme=officecharts.Theme(font=officecharts.Font(family="Trebuchet MS"),
+                                                       legend_position=None,
+                                                       axis_y_format="0.00%",
+                                                       grid_major_x=officecharts.Style(width=0.75),
                                                        plot_area=officecharts.Style(fill_colour="#cacaca20",
                                                                                     colour="#cacaca"))
                               )
